181514d8515e06ba69b011bf8497bdd94494b140
marat-karamian <marat_karamian@epam.com>
2018-05-14 16:30:17 +0300

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,14 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int[] vars = {1,3,5,8,10,15};
+		
+		int i=0;
+		while (i<vars.length) {
+			System.out.println(vars[i]);
+			i+=2;
+		}
+	}
+
+}
