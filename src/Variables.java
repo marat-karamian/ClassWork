c2eac2e3c45212d9d7f7b560bf6ed863e6741dda
marat-karamian <marat_karamian@epam.com>
2018-05-14 16:30:55 +0300

    /

--- src/Variables.java
+++ src/Variables.java
@@ -3,7 +3,7 @@ public class Variables {
 
 	public static void main(String[] args) {
 		int[] vars = {1,3,5,8,10,15};
-		
+		System.out.println();
 		int i=0;
 		while (i<vars.length) {
 			System.out.println(vars[i]);
